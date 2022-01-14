162a3a016b202d9158daadfb6f8cbd93f3de2bba
superfew <69841642+superfew@users.noreply.github.com>
2022-01-14 16:24:42 +0700

    Update os.c

--- os.c
+++ os.c
@@ -1,17 +1,18 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NUM_THREADS 3
+#define NUM_THREADS 3/*define number of threaad = 3*/
 
 int sum;
 void *runner(void *param);
 
 int main(int argc, char *argv[]){
-    pthread_t workers[NUM_THREADS]; 
-    pthread_attr_t attr [NUM_THREADS]; 
+    pthread_t workers[NUM_THREADS]; /* the thread identifire*/
+    pthread_attr_t attr[NUM_THREADS]; /* set of thread attributes*/
 
 
     if (argc > 3){
+    
         fprintf(stderr, "usage: few <integer value>\n");
         return -1;
     }
@@ -20,17 +21,17 @@ int main(int argc, char *argv[]){
             fprintf(stderr, "%d must be >= 0\n", atoi(argv[i]));
         }
         pthread_attr_init(&attr[i]);
-        pthread_create(&workers[i],&attr[i],runner,argv[i]);
+        pthread_create(&workers[i],&attr[i],runner,argv[i+1]);
         pthread_join(workers[i],NULL);
-        printf("\nRunner[%d",i+1);
+        printf("\nRunner[%d]",i+1);
         printf("\nsum[%d] = %d\n\n",i+1,sum);
         }
     }
-    
+    /*the thread will begin control in this funtion */
     void *runner(void *param){
         int i, upper = atoi(param);
         sum = 0;
-        for (i = 1; 1 <= upper; i++)
+        for (i = 1; i <= upper; i++)
             sum += i;
         pthread_exit(0);
 
